626dee92182a26d86e86bbcdfdc44fcd1bb87a3f
Giogap <giogap@gmail.com>
2023-05-31 12:55:49 -0500

    upload index

--- crud/server/index.js
+++ crud/server/index.js
@@ -1,5 +1,22 @@
 const express = require("express");
 const app = express();
+const mysql = require("mysql")
+
+const db = mysql.createConnection({
+    host:"localhost",
+    user:"root",
+    password:"",
+    database:"es_db"
+});
+
+app.post("/create",(req,res)=>{
+    const nombre = req.body.nombre;
+    const genero1 = req.body.genero1;
+    const genero2 = req.body.genero2;
+    const pais = req.body.pais;
+    const email = req.body.email;
+    const telefono = req.body.telefono;
+})
 
 app.listen(3001,()=>{
     console.log("run server")
