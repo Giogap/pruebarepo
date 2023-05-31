0afc19355302eddb633766954c09579770074d28
Giogap <giogap@gmail.com>
2023-05-31 12:45:30 -0500

    upload app and index

--- crud/admin/src/App.js
+++ crud/admin/src/App.js
@@ -1,23 +1,42 @@
-import logo from './logo.svg';
 import './App.css';
+import { useState } from "react"
 
 function App() {
+
+  const [nombre, setNombre] = useState("");
+  const [genero1, setGenero1] = useState("");
+  const [genero2, setGenero2] = useState("");
+  const [pais, setPais] = useState("");
+  const [correo, setCorreo] = useState("");
+  const [numero, setNumero] = useState(0);
+
+  const mostrarDatos = ()=>{
+    alert(nombre)
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="datos">
+        <label>Nombre Banda<input onChange={(event)=>{
+          setNombre(event.target.value);
+        }} type="text"></input></label>
+        <label>Genero 1<input onChange={(event)=>{
+          setGenero1(event.target.value);
+        }} type="text"></input></label>
+        <label>Genero 2<input onChange={(event)=>{
+          setGenero2(event.target.value);
+        }} type="text"></input></label>
+        <label>Pais<input onChange={(event)=>{
+          setPais(event.target.value);
+        }} type="text"></input></label>
+        <label>Correo<input onChange={(event)=>{
+          setCorreo(event.target.value);
+        }} type="email"></input></label>
+        <label>Numero Contacto<input onChange={(event)=>{
+          setNumero(event.target.value);
+        }} type="number"></input></label>
+        <button onClick={mostrarDatos}>Registrar</button>
+      </div>
     </div>
   );
 }
